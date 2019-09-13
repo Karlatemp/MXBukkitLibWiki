6877633fca1936f08503bdc058361af4d2f1425f
Karlatemp <3279826484@qq.com>
2019-09-13 23:28:40 +0800

    2.0

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ var Website = (function () {
     var title = r.querySelector(".root-title");
     console.log(pages, root);
     var sys = {};
+    /**
+     * @type {{[key: string]: Text}}
+     */
+    var doms = {};
     class Compon {
         /**
          * @returns {HTMLElement}
@@ -79,9 +83,16 @@ var Website = (function () {
             dd.append(down);
             down.style.display = "none";
             down.style.paddingLeft = "30px";
+            /**
+             * @type {{[key: string]: Text}}
+             */
+            this.doms = {};
         }
         onClick() {
-            var op = this.open = !this.open;
+            this.setOpen(!this.open);
+        }
+        setOpen(op) {
+            this.open = op;
             this.down.style.display = op ? "" : "none";
             this.ck.className = "button ico pagedown" + (op ? " active" : "");
         }
@@ -92,6 +103,9 @@ var Website = (function () {
         append(compon) {
             this.down.appendChild(compon.dom);
             compon.parent = this;
+            if (compon instanceof Text) {
+                this.doms[compon.name()] = compon;
+            }
             return this;
         }
     }
@@ -163,11 +177,132 @@ var Website = (function () {
                             }
                             return div;
                         }
+                        case "package": {
+                            var div = document.createElement("div");
+                            div.className = "make-package-view-pool";
+                            (function ($) {
+                                function a(v,/** @type {HTMLDivElement} */dv) {
+                                    if (v) {
+                                        switch (typeof v) {
+                                            case "string": {
+                                                var file = document.createElement("div");
+                                                file.className = "make-package-view-file";
+                                                dv.appendChild(file);
+                                                file.textContent = v;
+                                                break;
+                                            }
+                                            case "object": {
+                                                if (v instanceof Array) {
+                                                    for (var ov of v) {
+                                                        a(ov, dv);
+                                                    }
+                                                } else {
+                                                    for (var ky in v) {
+                                                        var ob = v[ky];
+                                                        var rt = document.createElement("div");
+                                                        rt.className = "make-package-view";
+                                                        var title = document.createElement("div");
+                                                        title.className = "make-package-view-title";
+                                                        title.textContent = ky;
+                                                        rt.appendChild(title);
+                                                        var c = document.createElement("div");
+                                                        c.className = "make-package-view-const";
+                                                        rt.appendChild(c);
+                                                        dv.appendChild(rt);
+                                                        a(ob, c);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                a($, div);
+                            })(values.pck);
+                            return div;
+                        }
                     }
                 }
             }
         }
     }
+    function pathenc(/** @type {string[]|string}*/path, mode) {
+        if (mode) {
+            // Encode
+            let buffer = String.fromCharCode(path.length);
+            for (const value of path) {
+                buffer += String.fromCharCode(value.length);
+                buffer += value;
+            }
+            return buffer;
+        } else {
+            /**
+             * @type {string[]}
+             */
+            let paths = [];
+            let size = path.charCodeAt(0);
+            for (let pos = 1, count = 0; count < size; count++) {
+                let len = path.charCodeAt(pos);
+                let cut = path.substr(pos + 1, len);
+                pos += len + 1;
+                paths.push(cut);
+            }
+            return paths;
+        }
+    }
+    function rep(/** @type {string} */value, mode) {
+        if (mode) {
+            return value.replace(/\%|\./gi, function (source) {
+                if (source == '.') {
+                    return '.,';
+                }
+                return '..';
+            });
+        } else {
+            return value.replace(/\.[\.\,]/gi, function (source) {
+                if (source == '..') return '%';
+                return '.';
+            });
+        }
+    }
+    function open(/** @type {string[]} */path, update) {
+        var rt = doms;
+        var com = null;
+        while (path.length) {
+            var lt = path.shift();
+            com = rt[lt];
+            if (com instanceof PageDown) {
+                rt = com.doms;
+                com.setOpen(true);
+            }
+        }
+        com.onClick();
+        if (update) {
+            awsl(pathenc(path, true), true);
+        }
+    };
+    sys.open = open;
+    function awsl(/** @type {string} */path, mode) {
+        if (mode) {
+            let encoded = rep(encodeURIComponent(path), 1);
+            history.pushState(null, null, "#" + encoded);
+        } else {
+            if (path != false) {
+                if (path.charAt(0) == '#') {
+                    path = path.substring(1);
+                }
+            }
+            if (path != false) {
+                let decoded = decodeURIComponent(rep(path, 0));
+                /**
+                 * @type {string[]}
+                 */
+                var pat = pathenc(decoded, false);
+                open(pat, false);
+            }
+        }
+    }
+    sys.hash = awsl;
+    sys.pathenc = pathenc;
     /**
      * @type {Show}
      */
@@ -192,10 +327,14 @@ var Website = (function () {
         onClick() {
             var title_ = this.name();
             let p = this.parent;
+            let path = [title_];
             while (p != null) {
-                title_ = p.name() + " > " + title_;
+                let pn = p.name();
+                path.unshift(pn);
+                title_ = pn + " > " + title_;
                 p = p.parent;
             }
+            awsl(pathenc(path, true), true);
             title.textContent = title_;
             var pg = this.getPage();
             pg.textContent = '';
@@ -224,6 +363,9 @@ var Website = (function () {
      */
     sys.append = (compon) => {
         pages.appendChild(compon.dom);
+        if (compon instanceof Text) {
+            doms[compon.name()] = compon;
+        }
         return sys;
     };
     /**
--- js/wiki.js
+++ js/wiki.js
@@ -1,6 +1,211 @@
 !(function () {
+    var tk = {
+        br() {
+            return document.createElement("br");
+        },
+        click(/** @type {HTMLElement} */element, handler) {
+            element.addEventListener("click", handler);
+            return element;
+        },
+        fake(value, chandler) {
+            var a = document.createElement("a");
+            a.href = "javascript:void(0)";
+            a.textContent = value;
+            if (chandler) {
+                a.addEventListener("click", chandler);
+            }
+            return a;
+        }
+    };
     var a = Website;
-    a.append(
+    var V1_0 = new a.PageDown().name("1.0");
+    var V2_0 = new a.PageDown().name("2.0");
+    var Tools = new a.PageDown().name("工具")
+        .append(new a.Show().name("lastest.log.gz读取").value(
+            function () {
+                var pool = document.createElement("div");
+                var fileSelector = document.createElement("input");
+                fileSelector.type = "file";
+                var WConsole = WebConsole();
+                pool.appendChild(WConsole.DOM);
+                pool.appendChild(fileSelector);
+                function ddd(/**@type {Uint8Array} */arr) {
+                    return String.fromCharCode.apply(null, arr);
+                }
+                fileSelector.addEventListener("change", function (ev) {
+                    if (fileSelector.value) {
+                        var file = fileSelector.files[0];
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            var data = e.target.result;
+                            console.log(data);
+                            /**
+                             * @type {Uint8Array}
+                             */
+                            var dec = pako.inflate(data);
+                            console.log(dec);
+                            WConsole.push(dec);
+                            /*var f = 0;
+                            while (true) {
+                                var i = dec.indexOf(10, f);
+                                if (i < 0) {
+                                    var arr = dec.subarray(f);
+                                    console.log(ddd(arr));
+                                    break;
+                                } else {
+                                    var arr = dec.subarray(f, i);
+                                    console.log(ddd(arr));
+                                    f = i + 1;
+                                }
+                            }*/
+                        };
+                        reader.readAsArrayBuffer(file);
+                    }
+                });
+                return pool;
+            }
+        ));
+    a.append(V2_0);
+    V2_0.append(
+        new a.Show().name("V2.0更新").value([
+            "MXBukkitLib(现在应该叫MXLib了)现在提供了全自动配置搜索模式, 以及BeanManager. 改用设计模式(Interface -> Impl)",
+            "很大程度上提供了可扩展性", tk.br(),
+            "-- 因为遗留原因 plugin.yml 的name字段还是 MXBukkitLib, 并且由于不知名原因本库的核心存储名字是 cn.mcres.karlatemp.MXBukkitLib"
+        ])
+    ).append(
+        new a.PageDown().name("全自动模块搜索:")
+            .append(new a.Show().name("Index").value([
+                "MXLib提供了全自动的模块搜索功能, 你可以使用它达到极高的可移植性",
+                "你所需要做的也就仅仅几步", tk.br(),
+                "首先创建一个项目, 把MXLib添加到依赖库", tk.br(),
+                "并且把你的项目配置成这样",
+                {
+                    type: "package", pck: {
+                        "META-INF": "mxlib.autoconfig.list",
+                        "cn.mcres.karlatemp.mxlib.example": ["ExampleAutoConfig.java", "ExamplePlugin.java"]
+                    }
+                },
+                "ExamplePlugin就是平常的插件主类,这里不再多说. 另外，我是把Bean当做模块看的所以这里的模块指的都是Bean"
+            ]))
+            .append(new a.Show().name("ExampleAutoConfig").value([
+                "ExampleAutoConfig 定义了一整套", tk.fake("BeanManager", function () {
+                    a.open(["2.0", "内置模块", "BeanManager"]);
+                }), "管理器, 我们可以在这里直接注册我们的模块! 请注意, 类名必须以 'AutoConfig' 结尾",
+                {
+                    type: 'code', mode: 'java', height: '500px', code: "\
+@Configuration\n\
+public class ExampleAutoConfig {\n\
+    ExampleAutoConfig(){} //必须具备默认构造器\n\n\
+    @Bean\n\
+    com.google.gson.Gson gson() { //注册Gson\n\
+        return new com.google.gson.Gson();\n\
+    }\n\n\
+    @Bean\n\
+    IToolkit my_toolkit() {// 设计模式: Interface -> Impl\n\
+        return new IToolkit(){public String run(){return \"IToolkit\";}}\n\
+    }\n\
+    @Bean\n\
+    void boot() {//从AutoConfig启动程序\n\
+        System.out.println(\"Boot from auto configuation.\");\n\
+    }\n\
+    @Bean\n\
+    MethodHandles.Lookup lookup(MethodHandles.Lookup old){\n\
+        // MethodHandles.Lookup old -> 在lookup之前储存在BeanManager的模块 \n\
+        // 可添加任意参数, 统一从BeanManager拿取\n\
+        if(old == null) old = MethodHandles.lookup();\n\
+        return old;\n\
+    }\n\
+}\n\ninterface IToolkit {\n    String run();\n}"}
+            ]))
+            .append(new a.Show().name("mxlib.autoconfig.list").value([
+                "mxlib.autoconfig.list 存储了AutoConfig的列表, 我们需要在这里添加注册的类", tk.br(),
+                "这是该文件的语法",
+                {
+                    type: 'code', mode: 'properties', height: '500px', code: '\
+# 所有以 \'#\' 开头的都是注释\n\
+# 下面的是我们的默认配置\n\
+cn.mcres.karlatemp.mxlib.DefaultAutoConfig\n\
+cn.mcres.karlatemp.mxlib.share.PluginAutoConfig\n\
+cn.mcres.karlatemp.mxlib.impl.ImplSetupAutoConfig\n\
+cn.mcres.gyhhy.MXLib.legacy.LegacyAutoConfig\n\
+# 直接写上类名, 我们就会前去搜索\n\n\
+cn.mcres.karlatemp.example.ExampleAutoConfig # 注释\n\
+# 这是错误的, 不允许在类名后面加 \'#\'\n\n\
+@/META-INF/mxlib.autoconfig.list.lowest\n\
+# 我们会二次加载以 \'@\' 开头的资源文件, 使用相同的语法\n\
+# 不过我们不建议, 因为这会搜索全部的 @.... 的内容\n\
+@/META-INF/mxlib.autoconfig.list.low\n\
+@/META-INF/mxlib.autoconfig.list.normal\n\
+@/META-INF/mxlib.autoconfig.list.high\n\
+@/META-INF/mxlib.autoconfig.list.highest\n\
+@/META-INF/mxlib.autoconfig.list.unmodifiable\n\
+# Tips: 如果你需要替换MXLib内部的Bean的话你的规则文件名需要改成 \'mxlib.autoconfig.list.lowest\'\n\
+#       不这样做的话会因为资源的搜索顺序问题导致可能无法正确执行\n\n\n\n\
+cn.mcres.karlatemp.mxlib.example.ExampleAutoConfig\n# cn.mcres.karlatemp.mxlib.example.ExampleAutoConfig\n# 不需要,因为会自动搜索子配置\n\
+# 如果你的类不是在一个jar里面的话那么是需要指定的\ncn.mcres.karlatemp.mxlib.example.otherjar.OtherAutoConfig\n\
+# 及时在同一个jar类如果没有匹配包(package)规则的话那么也不会执行, 需要指定\ncn.mcres.karlatemp.mxlib.package2.ExampleAutoConfig\n\
+# 包匹配规则\n\
+# 像是填入 \'cn.mcres.karlatemp.example.ExampleAutoConfig\'\n\
+# 搜索这个类的jar的全部类\n\
+#      cn.mcres.karlatemp.example.ExampleAutoConfig\n\
+#      cn.mcres.karlatemp.example.Example2AutoConfig\n\
+#      cn.mcres.karlatemp.example.Example3AutoConfig\n\
+#      cn.mcres.karlatemp.example.Example4AutoConfig\n\
+#      cn.mcres.karlatemp.example.ExampleAutoConfiguration\n\
+#      cn.mcres.karlatemp.exampleYouWhat.ExampleHAAutoConfig\n\
+#      cn.mcres.karlatemp.pack.xAutoConfig\n\
+#      cn.mcres.karlatemp.pack.yAutoConfig\n\
+#      cn.mcres.karlatemp.pack.zAutoConfig\n\
+#\n# 获取填入类的包 -> \'cn.mcres.karlatemp.example.\'\n\
+# 过滤掉不是以这个包名开头的类\n\
+#      cn.mcres.karlatemp.example.ExampleAutoConfig\n\
+#      cn.mcres.karlatemp.example.Example2AutoConfig\n\
+#      cn.mcres.karlatemp.example.Example3AutoConfig\n\
+#      cn.mcres.karlatemp.example.Example4AutoConfig\n\
+#      cn.mcres.karlatemp.example.ExampleAutoConfiguration\n\
+#  [-] cn.mcres.karlatemp.exampleYouWhat.ExampleHAAutoConfig\n\
+#  [-] cn.mcres.karlatemp.pack.xAutoConfig\n\
+#  [-] cn.mcres.karlatemp.pack.yAutoConfig\n\
+#  [-] cn.mcres.karlatemp.pack.zAutoConfig\n\
+# 忽略不是以 AutoConfig 结尾的类\n\
+#      cn.mcres.karlatemp.example.ExampleAutoConfig\n\
+#      cn.mcres.karlatemp.example.Example2AutoConfig\n\
+#      cn.mcres.karlatemp.example.Example3AutoConfig\n\
+#      cn.mcres.karlatemp.example.Example4AutoConfig\n\
+#  [*] cn.mcres.karlatemp.example.ExampleAutoConfiguration\n\
+'}
+            ]))
+            .append(new a.Show().name("资源(Bean)使用").value([
+                "我们注册了Bean, 那么我们应该要怎么样使用Bean呢, 第一种最简单粗暴的是获取BeanManager然后获取我们的Bean",
+                { type: 'code', height: '15px', mode: 'java', code: 'IToolkit kit = MXBukkitLib.getBeanManager().getBean(IToolkit.class);' },
+                "这样是最直接的并且Bean被覆盖更新也能拿到更新后的Bean, 但是这样的话还略显麻烦, 我们可以使用全自动模式加载, 首先这是我们的项目",
+                { type: 'package', pck: { 'META-INF': 'mxlib.autoconfig.list -> 内容 -> [cn.mcres.karlatemp.mxlib.example.EAutoConfig]', 'cn.mcres.karlatemp.mxlib': { example: ["EAutoConfig.java", "PluginClass.java", { sub: ['C1.java', 'C2.java'] }], test: ["Class1.java", "Class2.java"] } } },
+                "然后根据过滤器规则 (规则和autoconfig.list的规则一样，不过没有最后的忽略非AutoConfig), 下面的类会参与 Bean 获取 Task",
+                { type: 'package', pck: { 'cn.mcres.karlatemp.mxlib': { example: ["EAutoConfig.java", "PluginClass.java", { sub: ['C1.java', 'C2.java'] }] } } },
+                "# 其他的都一样:",
+                {
+                    type: 'code', height: "150px", mode: 'java', code: '\
+package .....;\n\n\
+public class C1{\n\
+    @Resource\n\
+    private static IToolkit toolkit;\n\
+    private static void $init(....){\n\
+        // 如果有 $init 方法的话将会自动调用 $init 方法, 参数和 AutoConfig的方法参数一样, 在@Resource注入完成后执行\n\
+        System.out.println("Class init.");\n\
+    }\n\
+}' },
+                "然后你就可以直接使用 C1.toolkit 进行操作了"
+            ]))
+    ).append(
+        new a.PageDown().name("内置模块")
+            .append(new a.Show().name("BeanManager").value([
+                "?"
+            ]))
+    )
+        ;
+    a.append(Tools);
+    a.append(V1_0);
+    V1_0.append(
         new a.PageDown().name("命令模块")
             .append(new a.Show().name("Index").value(
                 "MXBukkitLib 提供了快速构建命令的方法, 你可以以十分快的速度构建你自己的命令"
@@ -28,7 +233,7 @@ public class Test{\n\
     }\n\
 }"},
                 "很好,我们已经创建了一个或者多个子命令了,但是! 这并没有什么用, 因为你还没加载!",
-                document.createElement("br"), "假如你的 plugin.yml 长这样", {
+                tk.br(), "假如你的 plugin.yml 长这样", {
                     type: "code", mode: "yaml", height: "130px", code: "\
 name: Example\n\
 main: cn.example.Main\n\
@@ -218,7 +423,7 @@ WebHelper.post("https://www.example.com")\n\
             }
         ])).append(new a.Show().name("获取和使用").value([
             "我们需要调用", { type: 'code', mode: 'java', height: '30px', code: "cn.mcres.gyhhy.MXLib.yggdrasil.Yggdrasil.getServerYggdrasil();" },
-            "获取Yggdrasil实例,当然你也可以直接 new 一个实例", document.createElement("br"),
+            "获取Yggdrasil实例,当然你也可以直接 new 一个实例", tk.br(),
             "获取Yggdrasil实例后, 我们可以使用这些方法快速查询玩家信息, 查询玩家皮肤信息等, 下面是获取玩家皮肤的代码实例",
             {
                 type: 'code', mode: 'java', height: '180px', code: '\
@@ -262,7 +467,7 @@ if(ins == null){\n\
             }
         ]))
         .append(new a.PageDown().name("URL Protocol").append(new a.Show().name("主目录").value(["我们封装了些许数据方法, 这也许会对你有十分大的用处 这个区域的内容可以脱离Bukkit运行",
-            document.createElement("br"), "如果你是在 public static void main(String[] args) 运行的, 请运行此句",
+            tk.br(), "如果你是在 public static void main(String[] args) 运行的, 请运行此句",
             a.text(document.createElement("pre"), "cn.mcres.gyhhy.MXLib.http.URLStreamManager.load();")]))
             .append(new a.Show().name("Rcon").value(["RCON, 我们远程连接服务器所使用的协议, 十分熟悉吧", {
                 type: 'code', mode: 'java', height: '250px', code: '\
@@ -283,7 +488,7 @@ try(Closeable unuse = (Closeable)con){\n\
                     type: 'code', mode: 'java', height: '150px', code: '\
 URLStreamManager.load();\nInputStream stream = new URL("base64:UE9JJTIx").openStream();'}
             ])).append(new a.Show().name("Data").value([
-                "如果你是前端开发者的话, 你90%用过 data: 吧, 现在, 我们成功将它搬到了Java里面!", document.createElement("br"), {
+                "如果你是前端开发者的话, 你90%用过 data: 吧, 现在, 我们成功将它搬到了Java里面!", tk.br(), {
                     type: 'code', mode: 'java', height: '250px', code: '\
 URL u = new URL("data:" + datas); // 这将返还datas 的内容, 不过得保证里面没有 \',\'\n\n\
 URL u = new URL("data:,"+datas); // 这是推荐的方法,不会错误解析\n\n\
@@ -291,4 +496,8 @@ URL u = new URL("data:charset=gbk,"+datas); //将使用 gbk 编码\n\n\
 URL u = new URL("data:base64,"+base64); // 将使用base64解码内容, 可用于图片传输'}
             ])))
         ;
+})();
+(function () {
+    var web = Website;
+    web.hash(location.hash, false);
 })();
